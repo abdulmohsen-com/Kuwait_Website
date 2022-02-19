a13b53c67061e15e1f4a8d96ae415e948391c22a
abdulmohsen-com <66735197+abdulmohsen-com@users.noreply.github.com>
2022-02-19 11:26:44 +0300

    First Instalment, need a wireframe

---
+++ Script.js
@@ -0,0 +1,26 @@
+// var i = 0; //start point
+// var image = [];
+// var time = 3000;
+
+// //image list
+// image[0] = './images/Kuwait_Towers.jpg'
+// image[1] = './images/Underwater.jpg'
+
+
+// //change image
+
+// function changeImg(){
+//     document.Slide.src = image[i];
+//     if(i < image.length - 1){
+//         i++
+//     }else {
+//         i = 0;
+//     }
+
+
+//     setTimeout("changeImg()", time);
+
+// }
+
+
+//     window.onload = changeImg;
